8e06e9a0a429297a4189eaeb99b80f454f1f4be0
Yiğit Levent <yigitlevent@live.com>
2020-07-23 20:38:34 +0300

    i mean, it's basically a self promotion thing right now.

---
+++ README.md
@@ -0,0 +1,1 @@
+# yigitlevent.github.io
---
+++ precache-manifest.b400f3af15e57a9dd14f8aec3991fae6.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91b5f7ed3d6aca9dcdedf54a9c3e17a7",
+    "url": "./index.html"
+  },
+  {
+    "revision": "292921b7869d1f758edd",
+    "url": "./static/css/main.ede9130d.chunk.css"
+  },
+  {
+    "revision": "014b4e0f293f7785362f",
+    "url": "./static/js/2.c46b4663.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.c46b4663.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "292921b7869d1f758edd",
+    "url": "./static/js/main.54a15eb9.chunk.js"
+  },
+  {
+    "revision": "8466746706bbc9f62204",
+    "url": "./static/js/runtime-main.48ddeb0c.js"
+  },
+  {
+    "revision": "eb30aadedcf9f4566f8ac4334ea42a8a",
+    "url": "./static/media/Cambria.eb30aade.ttf"
+  },
+  {
+    "revision": "3d680d8da5187caea3542cd5acd0ed9a",
+    "url": "./static/media/me.3d680d8d.jpg"
+  }
+]);
