9b922628d30e96d6121a0bf9fecfe645ce5bd608
yigitlevent <yigitlevent@live.com>
2021-08-17 22:14:12 +0300

    Projects update

--- src/components/app/Projects.tsx
+++ src/components/app/Projects.tsx
@@ -17,26 +17,10 @@ export function Projects(): JSX.Element {
 
 			<p>WIP</p>
 
-			<Subtitle>Burning Wheel Gold Revised</Subtitle>
+			<Subtitle>Roleplaying</Subtitle>
 
-			<a href="./bwgr-distillation">BWGR Distillation</a>
-			<p>create and manage spells</p>
-
-			<a href="./bwgr-lifepaths">BWGR Lifepaths</a>
-			<p>an app to explore lifepaths</p>
-
-			<a href="./bwgr-factioncards">BWGR Faction Cards</a>
-			<p>create and manage faction cards</p>
-
-			<a href="./bwgr-magicwheel">BWGR Magic Wheel</a>
-			<p>spin the magic wheel</p>
-
-			<a href="./bwgr-unitcards">BWGR Unit Cards</a>
-			<p>create and manage unit cards</p>
-
-			<Subtitle>Kult</Subtitle>
-
-			<p>WIP</p>
+			<a href="./bwgr-tools">BWGR Tools</a>
+			<p>a collection of tools for burning wheel gold revised</p>
 		</Wrapper>
 	);
 }
